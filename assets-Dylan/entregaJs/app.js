b7135c1f69b89eb354a88b9eee9a6b92b9d01851
Dylan Pires <Dylan6018@gmail.com>
2024-11-12 23:19:41 -0300

    subiendo archivo de la preentregajss1

--- assets-Dylan/entregaJs/app.js
+++ assets-Dylan/entregaJs/app.js
@@ -1,16 +1,16 @@
 function verificarSalud() {
-    let haceEjercicio, comeSaludable;
+    let mayor, info;
     let intentos = 3;  // hasta tres  intentos
 
     // solo deja  hasta 3 intentos para responder correctamente
     for (let i = 0; i < intentos; i++) {
         // pregunta si hace es mayor y si quiere saber de nosotros
-        haceEjercicio = prompt("¿Tenes +18? (sí o no)").toLowerCase();
-        comeSaludable = prompt("¿Buscas informacion sobre nosotros? (sí o no)").toLowerCase();
+        mayor = prompt("¿Tenes +18? (sí o no)").toLowerCase();
+        info = prompt("¿Buscas informacion sobre nosotros? (sí o no)").toLowerCase();
 
         // Verifica si las respuestas son válidas
         // valide el tilde por las dudas, nose si es correcto 
-        if ((haceEjercicio === "sí" || haceEjercicio === "si") && (comeSaludable === "sí" || comeSaludable === "si")) {
+        if ((mayor === "sí" || mayor === "si") && (info === "sí" || info === "si")) {
             alert("¡Genial! Estás en el lugar indicado.");
             break 
         } else {
